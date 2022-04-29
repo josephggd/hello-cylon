813c76881280bc70d0fcce90f9e5e0e9f66e4959
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2022-04-29 06:19:03 -0500

    gitignore

---
+++ hello-backend/src/main/java/com/example/hellobackend/HelloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBackendApplication.class, args);
+    }
+
+}
---
+++ hello-backend/src/test/java/com/example/hellobackend/HelloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
