a011fc20ab41c8467d6a1f29bafd00bf18020b37
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2022-04-29 18:55:39 -0500

    First service + tests

---
+++ hello-backend/src/main/java/com/example/hellobackend/services/ToDoListService.java
@@ -0,0 +1,39 @@
+package com.example.hellobackend.services;
+
+import com.example.hellobackend.entities.ToDoList;
+import com.example.hellobackend.repositories.ToDoListRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+// The Service annotation, similar to the @Repository annotation, is used to mark a class as a service.
+@Service
+public class ToDoListService {
+    // Logger instantiated for the class to use
+    Logger logger = Logger.getLogger(ToDoListService.class.getName());
+
+    // Dictate dependency fields for TodoListService
+    private final ToDoListRepository toDoListRepository;
+
+    // Constructor calls on autowired fields
+    public ToDoListService(ToDoListRepository toDoListRepository) {
+        this.toDoListRepository = toDoListRepository;
+    }
+
+    // Get all to do lists
+    public List<ToDoList> findAllToDoLists() {
+        logger.info("Getting all to do lists");
+        // .findall() returns an iterable of ALL to do lists in the table
+        return toDoListRepository.findAll();
+    }
+
+    // Save a to do list
+    public ToDoList saveNewToDoList(ToDoList toDoList) {
+        logger.info("Saving new to do list");
+        // .save() returns the saved to do list
+        return toDoListRepository.save(toDoList);
+    }
+
+
+}
---
+++ hello-backend/src/test/java/com/example/hellobackend/services/ToDoListServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.hellobackend.services;
+
+import com.example.hellobackend.entities.ToDoList;
+import com.example.hellobackend.repositories.ToDoListRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@DisplayName("ToDoListService tests:")
+class ToDoListServiceTest {
+    private AutoCloseable autoCloseable;
+    @Mock
+    private ToDoListRepository toDoListRepository;
+    @InjectMocks
+    ToDoListService toDoListService;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        toDoListService = new ToDoListService(toDoListRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    @DisplayName("findAlLToDoLists() should return a list of ToDoLists")
+    void findAllToDoLists_success() {
+        // given
+        given(toDoListRepository.findAll()).willReturn(List.of(new ToDoList()));
+        // when
+        toDoListService.findAllToDoLists();
+        // then
+        verify(toDoListRepository).findAll();
+    }
+
+    @Test
+    @DisplayName("saveNewToDoList() should save a new ToDoList")
+    void saveNewToDoList_success() {
+        // given
+        ToDoList toDoList = new ToDoList();
+        // when
+        toDoListService.saveNewToDoList(toDoList);
+        // then
+        verify(toDoListRepository).save(toDoList);
+    }
+}
