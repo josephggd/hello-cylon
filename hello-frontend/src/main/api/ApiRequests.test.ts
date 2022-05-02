baa7bb0ca54fae81e04abe9b5ad9e98ae79afad0
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2022-05-02 14:09:09 -0500

    added PUT/delete/tests

--- hello-frontend/src/main/api/ApiRequests.test.ts
+++ hello-frontend/src/main/api/ApiRequests.test.ts
@@ -1,27 +1,47 @@
 import {ToDoList} from "../../dtos/ToDoList";
 import axios from "axios";
-import {postNewToDoList, getAllToDoLists, putUpdateToDoList, deleteToDoList, deleteToDoItem} from "./ApiRequests";
+import {
+  postNewToDoList,
+  getAllToDoLists,
+  putUpdateToDoList,
+  deleteToDoList,
+  deleteToDoItem,
+  postNewToDoItem,
+  putUpdateToDoItem,
+  getListsUrl,
+  postNewListUrl,
+  putListUrl,
+  deleteItemUrl,
+  putItemUrl,
+  deleteListUrl,
+  postNewItemUrl,
+  failedToDelete,
+  failedToPut,
+  failedToPost
+} from "./ApiRequests";
 import {ToDoListJson} from "../../jsons/ToDoListJson";
+import {ToDoItem} from "../../dtos/ToDoItem";
 jest.mock("axios");
 
+const toDoItem: ToDoItem = {
+  id: null,
+  title: "title",
+  description: "description",
+};
+const toDoList: ToDoList = {
+  id: null,
+  title: "title",
+  description: "description",
+  items: [
+    toDoItem,
+  ],
+};
+const toDoLists: ToDoList[] = [toDoList];
 // All tests run w/in the describe function
 describe("successful API requests", () => {
   // Mocking axios/any other modules in use is required for testing
   const mockedAxios = axios as jest.Mocked<typeof axios>;
   // Creating constants to use throughout tests (don't need to create multiple instances)
-  const toDoList: ToDoList = {
-    id: null,
-    title: "title",
-    description: "description",
-    items: [
-      {
-        id: null,
-        title: "title",
-        description: "description",
-      },
-    ],
-  };
-  const toDoLists: ToDoList[] = [toDoList];
   it("GET request is made properly", async () => {
     // Mock the response
     const mockedResponse = { status:200, data:toDoLists };
@@ -30,36 +50,50 @@ describe("successful API requests", () => {
     // Perform the API call in a mock context
     const returnedData = await getAllToDoLists();
     // Capture the API request's endpoint
-    expect(mockedAxios.get).toHaveBeenCalledWith("localhost:9090/data");
+    expect(mockedAxios.get).toHaveBeenCalledWith(getListsUrl);
     // Compare data to mock response
     expect(returnedData).toBe(mockedResponse.data);
   });
-  it("POST request is made properly", async () => {
+  it("POST list request is made properly", async () => {
     const mockResponse = { status:200 };
     mockedAxios.post.mockResolvedValue(mockResponse);
     const returnedData = await postNewToDoList(toDoList);
-    expect(mockedAxios.post).toHaveBeenCalledWith("localhost:9090/data", toDoList);
+    expect(mockedAxios.post).toHaveBeenCalledWith(postNewListUrl, toDoList);
+    expect(returnedData).toBe("SAVED");
+  });
+  it("POST item request is made properly", async () => {
+    const mockResponse = { status:200 };
+    mockedAxios.post.mockResolvedValue(mockResponse);
+    const returnedData = await postNewToDoItem(toDoItem);
+    expect(mockedAxios.post).toHaveBeenCalledWith(postNewItemUrl, toDoItem);
     expect(returnedData).toBe("SAVED");
   });
   it("PUT request is made properly", async () => {
     const mockResponse = { status:200 };
     mockedAxios.put.mockResolvedValue(mockResponse);
     const returnedData = await putUpdateToDoList({...toDoList, id: 1});
-    expect(mockedAxios.put).toHaveBeenCalledWith("localhost:9090/data", {...toDoList, id: 1});
+    expect(mockedAxios.put).toHaveBeenCalledWith(putListUrl, {...toDoList, id: 1});
+    expect(returnedData).toBe("UPDATED");
+  });
+  it("PUT item request is made properly", async () => {
+    const mockResponse = { status:200 };
+    mockedAxios.put.mockResolvedValue(mockResponse);
+    const returnedData = await putUpdateToDoItem({...toDoItem, id: 1});
+    expect(mockedAxios.put).toHaveBeenCalledWith(putItemUrl, {...toDoItem, id: 1});
     expect(returnedData).toBe("UPDATED");
   });
   it("DELETE LIST request is made properly", async () => {
     const mockResponse = { status:200 };
     mockedAxios.delete.mockResolvedValue(mockResponse);
     const returnedData = await deleteToDoList(1 );
-    expect(mockedAxios.delete).toHaveBeenCalledWith("localhost:9090/list/1");
+    expect(mockedAxios.delete).toHaveBeenCalledWith(deleteListUrl+"1");
     expect(returnedData).toBe("DELETED");
   });
   it("DELETE ITEM request is made properly", async () => {
     const mockResponse = { status:200 };
     mockedAxios.delete.mockResolvedValue(mockResponse);
     const returnedData = await deleteToDoItem(1 );
-    expect(mockedAxios.delete).toHaveBeenCalledWith("localhost:9090/item/1");
+    expect(mockedAxios.delete).toHaveBeenCalledWith(deleteItemUrl+"1");
     expect(returnedData).toBe("DELETED");
   });
 });
@@ -67,52 +101,54 @@ describe("failed API requests", () => {
   // Mocking axios/any other modules in use is required for testing
   const mockedAxios = axios as jest.Mocked<typeof axios>;
   // Creating constants to use throughout tests (don't need to create multiple instances)
-  const toDoList: ToDoList = {
-    id: null,
-    title: "title",
-    description: "description",
-    items: [
-      {
-        id: null,
-        title: "title",
-        description: "description",
-      },
-    ],
-  };
   const toDoListJson:ToDoListJson = {toDoLists: []};
   it("GET request is made properly", async () => {
     const mockedResponse = { data:toDoListJson };
     mockedAxios.get.mockResolvedValue(mockedResponse);
     const returnedData = await getAllToDoLists();
-    expect(mockedAxios.get).toHaveBeenCalledWith("localhost:9090/data");
+    expect(mockedAxios.get).toHaveBeenCalledWith(getListsUrl);
     expect(returnedData).toEqual(toDoListJson);
   });
-  it("POST request fails", async () => {
+  it("POST list request fails", async () => {
     const mockResponse = { status:400 };
     mockedAxios.post.mockResolvedValue(mockResponse);
     const returnedData = await postNewToDoList(toDoList);
-    expect(mockedAxios.post).toHaveBeenCalledWith("localhost:9090/data", toDoList);
-    expect(returnedData).toBe("POST request failed");
+    expect(mockedAxios.post).toHaveBeenCalledWith(postNewListUrl, toDoList);
+    expect(returnedData).toBe(failedToPost);
   });
-  it("PUT request fails", async () => {
+  it("POST item request fails", async () => {
+    const mockResponse = { status:400 };
+    mockedAxios.post.mockResolvedValue(mockResponse);
+    const returnedData = await postNewToDoItem(toDoList);
+    expect(mockedAxios.post).toHaveBeenCalledWith(postNewItemUrl, toDoList);
+    expect(returnedData).toBe(failedToPost);
+  });
+  it("PUT list request fails", async () => {
     const mockResponse = { status:400 };
     mockedAxios.put.mockResolvedValue(mockResponse);
     const returnedData = await putUpdateToDoList({...toDoList, id: 1});
-    expect(mockedAxios.put).toHaveBeenCalledWith("localhost:9090/data", {...toDoList, id: 1});
-    expect(returnedData).toBe("PUT request failed");
+    expect(mockedAxios.put).toHaveBeenCalledWith(putListUrl, {...toDoList, id: 1});
+    expect(returnedData).toBe(failedToPut);
+  });
+  it("PUT item request fails", async () => {
+    const mockResponse = { status:400 };
+    mockedAxios.put.mockResolvedValue(mockResponse);
+    const returnedData = await putUpdateToDoItem({...toDoItem, id: 1});
+    expect(mockedAxios.put).toHaveBeenCalledWith(putItemUrl, {...toDoItem, id: 1});
+    expect(returnedData).toBe(failedToPut);
   });
   it("DELETE LIST request fails", async () => {
     const mockResponse = { status:400 };
     mockedAxios.delete.mockResolvedValue(mockResponse);
     const returnedData = await deleteToDoList(1 );
-    expect(mockedAxios.delete).toHaveBeenCalledWith("localhost:9090/list/1");
-    expect(returnedData).toBe("DELETE request failed");
+    expect(mockedAxios.delete).toHaveBeenCalledWith(deleteListUrl+"1");
+    expect(returnedData).toBe(failedToDelete);
   });
   it("DELETE ITEM request fails", async () => {
     const mockResponse = { status:400 };
     mockedAxios.delete.mockResolvedValue(mockResponse);
     const returnedData = await deleteToDoItem(1 );
-    expect(mockedAxios.delete).toHaveBeenCalledWith("localhost:9090/item/1");
-    expect(returnedData).toBe("DELETE request failed");
+    expect(mockedAxios.delete).toHaveBeenCalledWith(deleteItemUrl+"1");
+    expect(returnedData).toBe(failedToDelete);
   });
 });
--- hello-frontend/src/main/api/ApiRequests.ts
+++ hello-frontend/src/main/api/ApiRequests.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
 import {ToDoList} from "../../dtos/ToDoList";
 import {ToDoListJson} from "../../jsons/ToDoListJson";
-const failedToPut = "PUT request failed";
-const failedToPost = "POST request failed";
-const failedToGet = "GET request failed";
-const failedToDelete = "DELETE request failed";
+import {ToDoItem} from "../../dtos/ToDoItem";
 
+export const failedToPut = "PUT request failed";
+export const failedToPost = "POST request failed";
+export const failedToGet = "GET request failed";
+export const failedToDelete = "DELETE request failed";
+export const baseUrl = "http://localhost:9090/";
+export const postNewListUrl = baseUrl + "save/list";
+export const postNewItemUrl = baseUrl + "save/item";
+export const getListsUrl = baseUrl + "all";
+export const putListUrl = baseUrl + "update/list";
+export const putItemUrl = baseUrl + "update/item";
+export const deleteListUrl = baseUrl + "remove/list/";
+export const deleteItemUrl = baseUrl + "remove/item/";
 // Export an asynchronous function that makes a GET request & returns response.data
 export async function getAllToDoLists():Promise<ToDoListJson> {
   try {
     // axios makes a GET request to the specified URL
-      const response = await axios.get("localhost:9090/data");
+      const response = await axios.get(getListsUrl);
       // If the response is successful, return the response data
       if (response.status === 200) {
           return response.data;
@@ -26,12 +35,25 @@ export async function getAllToDoLists():Promise<ToDoListJson> {
 
 export async function postNewToDoList(newToDoList : ToDoList):Promise<string> {
   try {
-      const response = await axios.post("localhost:9090/data", newToDoList);
-      if (response.status===200){
-        return "SAVED";
-      } else {
-        throw new Error(failedToPost);
-      }
+    const response = await axios.post(postNewListUrl, newToDoList);
+    if (response.status===200){
+      return "SAVED";
+    } else {
+      throw new Error(failedToPost);
+    }
+  } catch {
+    return failedToPost;
+  }
+}
+
+export async function postNewToDoItem(newToDoItem: ToDoItem):Promise<string> {
+  try {
+    const response = await axios.post(postNewItemUrl, newToDoItem);
+    if (response.status===200){
+      return "SAVED";
+    } else {
+      throw new Error(failedToPost);
+    }
   } catch {
     return failedToPost;
   }
@@ -39,7 +61,20 @@ export async function postNewToDoList(newToDoList : ToDoList):Promise<string> {
 
 export async function putUpdateToDoList(existingToDoList : ToDoList):Promise<string> {
   try {
-    const response = await axios.put("localhost:9090/data", existingToDoList);
+    const response = await axios.put(putListUrl, existingToDoList);
+    if (response.status===200){
+      return "UPDATED";
+    } else {
+      throw new Error(failedToPut);
+    }
+  } catch {
+    return failedToPut;
+  }
+}
+
+export async function putUpdateToDoItem(existingToDoItem: ToDoItem):Promise<string> {
+  try {
+    const response = await axios.put(putItemUrl, existingToDoItem);
     if (response.status===200){
       return "UPDATED";
     } else {
@@ -52,7 +87,7 @@ export async function putUpdateToDoList(existingToDoList : ToDoList):Promise<str
 
 export async function deleteToDoList(id : number):Promise<string> {
   try {
-    const response = await axios.delete("localhost:9090/list/"+id);
+    const response = await axios.delete(deleteListUrl+id);
     if (response.status===200){
       return "DELETED";
     } else {
@@ -65,7 +100,7 @@ export async function deleteToDoList(id : number):Promise<string> {
 
 export async function deleteToDoItem(id : number):Promise<string> {
   try {
-    const response = await axios.delete("localhost:9090/item/"+id);
+    const response = await axios.delete(deleteItemUrl+id);
     if (response.status===200){
       return "DELETED";
     } else {
